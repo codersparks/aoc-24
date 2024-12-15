03e5c552d0d8a4bf96feb5ff32a1cb9ec1dc6c99
codersparks <codersparks@users.noreply.github.com>
2024-12-15 13:07:31 +0000

    Day 6 - Implemented Maze generation

---
+++ aoc-24-day-6/src/guard.rs
@@ -0,0 +1,67 @@
+#[derive(Debug, Clone, Copy)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+
+impl Direction {
+    pub fn from_char(c: char) -> Direction {
+        match c {
+            '>' => Direction::Right,
+            '<' => Direction::Left,
+            '^' => Direction::Up,
+            'v' => Direction::Down,
+            _ => panic!("Invalid direction: {}", c)
+        }
+    }
+
+    pub fn representation(&self) -> char {
+        match self {
+            Direction::Up => { '^'}
+            Direction::Down => { 'v' }
+                Direction::Left => { '<' }
+                Direction::Right => { '>' }
+            }
+        }
+}
+
+#[derive(Debug)]
+pub struct GuardLocation {
+    direction: Direction,
+    row: usize,
+    col: usize,
+}
+
+impl GuardLocation {
+    pub fn new(direction: Direction, row: usize, col: usize) -> GuardLocation {
+        Self { direction, row, col }
+    }
+}
+
+#[derive(Debug)]
+pub struct Guard {
+    position: GuardLocation,
+    history: Vec<GuardLocation>,
+}
+
+impl Guard {
+
+    pub fn new(direction: Direction, row: usize, col: usize) -> Guard {
+        Self {
+            position: GuardLocation::new(direction, row, col),
+            history: vec![]
+        }
+    }
+
+    pub fn get_direction(&self) -> &Direction {
+        &self.position.direction
+    }
+
+
+}
+
+
+
